492672c5ad8553accd9904029e762fb06d13b8ea
yliao <superyao12@sina.com>
2017-09-18 23:10:55 +0800

    http请求拦截器

--- src/http/httpResource.js
+++ src/http/httpResource.js
@@ -4,28 +4,39 @@
 import Axios from 'axios'
 import { Loading, Message } from 'element-ui'
 
-// 设置默认请求头
-Axios.defaults.headers = {
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-  'X-Requested-With': 'XMLHttpRequest',
-  'Access-Control-Allow-Origin': '*'
-}
+function plugin (Vue) {
+  if (plugin.installed) {
+    return
+  }
+  // 设置默认请求头
+  Axios.defaults.headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Access-Control-Allow-Origin': '*'
+  }
 
-/**
- * 添加一个请求拦截器
- */
-Axios.interceptors.request.use(function (config) {
-  if (config.data) {
-    config.data = Object.assign({}, config.data)
-    for (let i of Object.keys(config.data)) {
-      if (config.data[i] instanceof Date) {
-        config.data[i] = new Date(config.data[i].getTime()).Format('yyyy-MM-dd hh:mm')
+  /**
+   * 添加一个请求拦截器
+   */
+  Axios.interceptors.request.use(function (config) {
+    if (config.data) {
+      config.data = Object.assign({}, config.data)
+      for (let i of Object.keys(config.data)) {
+        if (config.data[i] instanceof Date) {
+          config.data[i] = new Date(config.data[i].getTime()).Format('yyyy-MM-dd hh:mm')
+        }
       }
     }
-  }
-  Loading.service({fullscreen: true})
-  return config
-}, function (error) {
-  Message({showClose: true, message: '请求失败', type: 'error'})
-  return Promise.reject(error)
-})
+    Loading.service({fullscreen: true})
+    return config
+  }, function (error) {
+    Message({showClose: true, message: '请求失败', type: 'error'})
+    return Promise.reject(error)
+  })
+  Vue.http = Axios
+  Vue.prototype.$http = Axios
+}
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+export default plugin

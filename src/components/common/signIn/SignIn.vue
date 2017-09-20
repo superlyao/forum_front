54db1cdbcbfc584e70894b3ec728aecf637e36ef
yliao <superyao12@sina.com>
2017-09-20 22:21:57 +0800

    登录框

---
+++ src/components/common/signIn/SignIn.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-dialog v-model="" size="small">
+    <el-form v-model="signInParam">
+      <el-form-item label="用户名">
+        <el-input v-model="signInParam.userName" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="signInParam.passWord"  placeholder="请输入密码"></el-input>
+      </el-form-item>
+    </el-form>
+    <div class="text-right">
+      <el-button>确定</el-button>
+      <el-button>取消</el-button>
+    </div>
+  </el-dialog>
+</template>
+<script>
+  import { initSigninParam } from './signIn'
+  export default {
+    prop: ['signInDialog'],
+    components: {},
+    data () {
+      return {
+        signInParam: Object.assign({}, initSigninParam)
+      }
+    }
+  }
+</script>
---
+++ src/components/common/signIn/signIn.js
@@ -0,0 +1,7 @@
+/**
+ * Created by liaoyao on 2017/9/20.
+ */
+export const initSigninParam = {
+  userName: '',
+  passWord: ''
+}

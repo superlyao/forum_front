2768e8995eef297898a9d75e2676a365deff6078
yliao <superyao12@sina.com>
2017-12-17 21:10:37 +0800

    显示区域组件和默认组件

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>{{msg}}</div>
 </template>
 
 <script>
--- src/components/mainFramework/Container.vue
+++ src/components/mainFramework/Container.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <el-row class="page-padding">
+    <el-col :xs="24" :sm="24" :md="24" :lg="24">
+      <transition>
+        <router-view></router-view>
+      </transition>
+    </el-col>
+  </el-row>
 </template>
 <script>
   export default {
@@ -8,3 +14,14 @@
     }
   }
 </script>
+<style>
+  .page-padding {
+    padding: 0 3%;
+  }
+
+  @media (max-width: 992px) {
+    .page-padding {
+      padding: 0 0;
+    }
+  }
+</style>

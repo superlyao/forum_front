8eae773f573add9da82477c9cc792afd7f25a38d
yliao <superyao12@sina.com>
2017-12-20 21:32:40 +0800

    修改eslint规则和首页显示

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   env: {
     browser: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
@@ -22,6 +22,11 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    //禁用var，用let和const代替
+    'no-var': 0,
+    //强制驼峰法命名
+    'camelcase':2,
+    'space-before-function-paren': [0,'always']
   }
 }

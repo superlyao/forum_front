80a709acb321b58bd8a2ad92c4b1ec61d98953ea
yliao <superyao12@sina.com>
2018-01-15 21:11:42 +0800

    update package.json

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,6 +1,9 @@
 require('./check-versions')()
 
 var config = require('../config')
+const fs = require('fs')
+// 获取解析后的参数并转换格式
+const argv = require('optimist').argv
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
@@ -8,9 +11,11 @@ if (!process.env.NODE_ENV) {
 var opn = require('opn')
 var path = require('path')
 var express = require('express')
+//作为中间件用来解析http请求体（参数），是express默认使用的中间件之一
+var bodyParser = require('body-parser')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = (process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'production')
+var webpackConfig = process.env.NODE_ENV === 'testing'
   ? require('./webpack.prod.conf')
   : require('./webpack.dev.conf')
 
@@ -31,22 +36,56 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: false,
-  heartbeat: 2000
+  log: () => {}
 })
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    hotMiddleware.publish({action: 'reload'})
     cb()
   })
 })
 
+// mock/proxy api requests
+// if use proxy, set ./dev-server.js -proxy www.alm.com(proxy url)
+if (argv.t !== 'sysDebug') {
+  app.use(bodyParser())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+  app.use(bodyParser.json({
+    type: 'application/*+json'
+  }))
+}
+const mockDir = path.resolve(__dirname, '../mock');
+//遍历mock文件夹下所以js文件，并使用proxy转发请求
+(function mockProxy (mockDir) {
+  fs.readdirSync(mockDir).forEach(function (file) {
+    if (file !== 'utils.js' && file.indexOf('.json') < 1) {
+      let filePath = path.resolve(mockDir, file)
+      if (fs.statSync(filePath).isDirectory()) {
+        mockProxy(filePath)
+      }
+      else {
+        let mock = require(filePath)
+        if (argv.t !== 'sysDebug') {
+          app.use(mock.api, function (req, res, next) {
+            mock.response(req, res, fs)
+          })
+        } else {
+          //请求转发，可跨域
+          app.use(mock.api, proxyMiddleware({target: argv.p}))
+        }
+      }
+    }
+  })
+})(mockDir)
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options }
+    options = {target: options}
   }
   app.use(proxyMiddleware(options.filter || context, options))
 })
@@ -65,28 +104,20 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+var uri = 'http://localhost:' + port + '/main.html'
 
-var _resolve
-var readyPromise = new Promise(resolve => {
-  _resolve = resolve
+devMiddleware.waitUntilValid(function () {
+  console.log('> Listening at ' + uri + '\n')
 })
 
-console.log('> Starting dev server...')
-devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri)
   }
-  _resolve()
 })
-
-var server = app.listen(port)
-
-module.exports = {
-  ready: readyPromise,
-  close: () => {
-    server.close()
-  }
-}
